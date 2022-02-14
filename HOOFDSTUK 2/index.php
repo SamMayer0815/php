59e4d13e0a262fbf72c2d79c095951b8c7315c84
SamMayer0815 <90177055+SamMayer0815@users.noreply.github.com>
2022-02-14 09:38:14 +0100

    Add files via upload

---
+++ HOOFDSTUK 2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $a = "Ik heb niet gefaald";
+    $b = "die niet werken";
+    $c = 10000;
+    echo($a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . ". <br><br>");
+
+    $a = "die geen fout";
+    $b = "en";
+    $c = "niet.";
+    echo("Mensen " .$a .$b ." maken, werken " .$c ."<br><br>");
+
+    $a = "ry";
+    $b = ", but";
+    $c = "fail";
+    echo("T" .$a ." and " .$c .$b ." never " .$c ." to t" .$a ."<br><br>");
+
+    $a = "\"omgaan";
+    $b = "met";
+    $c = "teleu";
+    echo("De cursus " .$a ." " .$b ." " .$c ."rstelling\" kan vanavond helaas niet doorgaan.")
+
+
+
+  ?>
+  
+</body>
+</html>
